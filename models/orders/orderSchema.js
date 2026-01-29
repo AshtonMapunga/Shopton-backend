d31c2c080b81dc606de0d2f44ed9b0c24dffcd34
AshtonMapunga <ashtonmapunga0912@gmail.com>
2026-01-29 21:21:08 +0200

    updating order model

--- models/orders/orderSchema.js
+++ models/orders/orderSchema.js
@@ -1,27 +1,57 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  orderNumber: { type: Number }, // Make not required
-  orderNumberFormatted: { type: String }, // Make not required
+  orderNumber: { type: Number },
+  orderNumberFormatted: { type: String },
+
+  userId: { type: String },
+
+  deliveryDetails: {
+    fullName: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    address: { type: Number, required: true },
+    city: { type: Number, required: true }
+  },
 
   items: [
     {
       productId: { type: Number, required: true },
       name: { type: String, required: true },
-          productImage: { type: String, required: true },
-
+      productImage: { type: String, required: true },
       quantity: { type: Number, required: true },
-      price: { type: Number, required: true },
+      price: { type: Number, required: true }
     }
   ],
+
   totalAmount: { type: Number, required: true },
-  orderStatus: { type: String, enum: ['completed', 'pending', 'cancelled'], required: true },
+
+  paymentStatus: {
+    type: String,
+    enum: ['pending', 'paid', 'cancelled'],
+    default: 'pending'
+  },
+
+  deliveryStatus: {
+    type: String,
+    enum: ['completed', 'off', 'confirmed', 'processing', 'delivered'],
+    default: 'off'
+  },
+
+  paymentModel: {
+    type: String,
+    enum: ['bnpl', 'cod', 'online'],
+    default: 'online'
+  },
+
+  depositAmount: { type: Number, required: true },
+  remainAmount: { type: Number, required: true },
+
   paymentMethod: { type: String, required: true },
-  transactionReferenceID: { type: String, required: true },
+  transactionReferenceID: { type: String },
+
   createdAt: { type: Date, default: Date.now }
 });
-
 
 orderSchema.methods.toJSON = function () {
   const obj = this.toObject();
